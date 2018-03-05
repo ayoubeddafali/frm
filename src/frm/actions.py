73b86473fe5edc6605e7ca14fdc6e0faf4bb14e2
ayoubensalem <ayoubensalem@gmail.com>
2018-03-05 14:01:22 +0000

    update actions

--- src/frm/actions.py
+++ src/frm/actions.py
@@ -2,37 +2,57 @@ import os
 from git import Repo
 import shutil
 import sys
-import helper
-
-java_repo = "https://github.com/ayoubensalem/docker-java.git"
+from frm import helper
+repos = {
+    "java": "https://github.com/ayoubensalem/docker-java.git",
+    "default": "https://github.com/ayoubensalem/docker-java.git",
+    "php" :"",
+    "python": ""
+}
 tmp_path = "/tmp/docker/"
 DESTINATION_EXIST_CODE = 128
+PROJECT_TYPE = "default"
 
-def get_workflow(project_type):
+def get_workflow(project_type = "java"):
+    PROJECT_TYPE = project_type
     cwd = os.getcwd()
-    if project_type == "java":
-        if os.path.isdir(cwd + "/docker"):
-            print("'docker' folder already exists !!")
-        else :
-            try :
-                Repo.clone_from(java_repo, tmp_path + project_type )
+    if os.path.isdir(cwd + "/docker"):
+        print("'docker' folder already exists !!")
+    else :
+        try :
+            Repo.clone_from(repos[project_type], tmp_path + project_type )
+            shutil.copytree(tmp_path + project_type, cwd + "/docker")
+        except Exception as e:
+            if e.status == DESTINATION_EXIST_CODE:
                 shutil.copytree(tmp_path + project_type, cwd + "/docker")
-            except Exception as e:
-                if e.status == DESTINATION_EXIST_CODE:
-                    shutil.copytree(tmp_path + project_type, cwd + "/docker")
-                else :
-                    print("An error has occured, try later")
-                    sys.exit(1)
-    elif project_type == "php":
-        pass
-    elif project_type == "python":
-        pass
+            else :
+                print("An error has occured, try later")
+                sys.exit(1)
+
 
 def clean():
-    pass
+    cwd = os.getcwd()
+    if os.path.isdir(cwd + "/docker"):
+        shutil.rmtree(cwd + "/docker")
+    try :
+        Repo.clone_from(repos[PROJECT_TYPE], tmp_path + PROJECT_TYPE )
+        shutil.copytree(tmp_path + PROJECT_TYPE, cwd + "/docker")
+        print("Project was cleaned successfully !!")
+    except Exception as e:
+        if e.status == DESTINATION_EXIST_CODE:
+            shutil.copytree(tmp_path + PROJECT_TYPE, cwd + "/docker")
+            print("Project was cleaned successfully !!")
+        else :
+            print("An error has occured, try later")
+            sys.exit(1)
+
 
 def destroy():
-    pass
+    cwd = os.getcwd()
+    if os.path.isdir(cwd + "/docker"):
+        shutil.rmtree(cwd + "/docker")
+        print("CD workflow was removed successfully !! ")
+
 
 def get_infos():
     with open("info.txt") as f:
